7829820c8ccefdbcf20c8420178a0844d9bb1c8a
franzinBr <alan-franzin2011@live.com>
2021-05-31 14:37:38 -0400

    route for user to get all their URLs

---
+++ controllers/user.js
@@ -0,0 +1,18 @@
+const ErrorResponse = require("../utils/errorResponse")
+const Url = require('../models/Url')
+
+exports.getAllurlShortner = async (req, res, next) => {
+    try {
+        const urls = await Url.find({user: req.user}, '_id completeUrl code clicks')
+        if(!urls) return next(new ErrorResponse("this user has no url", 204))
+
+        res.status(200).json({
+            success: true,
+            urls
+        })
+        
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { getAllurlShortner } = require('../controllers/user');
+const {isAuth} = require('../middlewares/auth')
 
-
+router.route('').get(isAuth, getAllurlShortner)
 
 module.exports = router;
