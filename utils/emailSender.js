56e0d27243fcd8e14a4491d9558d9de1ea2e1aaa
franzinBr <alan-franzin2011@live.com>
2021-06-01 20:57:11 -0400

    add email sender service

---
+++ utils/emailSender.js
@@ -0,0 +1,23 @@
+const {createTransport} = require('nodemailer');
+
+const emailSender = (options) =>
+{
+    const transporter = createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {user: process.env.EMAIL_USERNAME, pass: process.env.EMAIL_PASSWORD}
+    })
+
+    const mailOptions = {
+        from: process.env.EMAIL_FROM,
+        to: options.to,
+        subject: options.subject,
+        html: options.text
+    }
+
+    transporter.sendMail(mailOptions, function(err, info){
+        if(err) console.log(err)
+        else console.log(info)
+    })
+}
+
+module.exports = emailSender;
