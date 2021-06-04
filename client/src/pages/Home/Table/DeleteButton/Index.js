aee94aee83d6b0987bf28aa8e72c6e7855aaa41c
franzinBr <alan-franzin2011@live.com>
2021-06-04 17:13:19 -0400

    created deleteButton component

--- client/src/pages/Home/Table/DeleteButton/Index.js
+++ client/src/pages/Home/Table/DeleteButton/Index.js
@@ -2,16 +2,19 @@ import React from 'react'
 import styles from './styles.module.css'
 
 const DeleteButton = () => {
-    const {showConfirm, setShowConfirm} = React.useState(false)
+    const [showConfirm, setShowConfirm] = React.useState(false)
 
-
+    function handleClick()
+    {
+        // delete url here
+    }
     return (
         <div className={styles.btn}>
-            {showConfirm && <div className={styles.confirm}>
-                <button>Yes</button>
-                <button>Cancel</button>
+            {showConfirm && <div className={`${styles.confirm} animeLeft`}>
+                <button className={styles.btnConfirm} onClick={handleClick}>Confirm</button>
+                <button className={styles.btnCancel} onClick={() => setShowConfirm(false)}>Cancel</button>
             </div>}
-            <div className={styles.delete}>Delete</div>
+            {!showConfirm && <div onClick={() => setShowConfirm(true)} className={`${styles.delete} animeRight`}>Delete</div>}
         </div>
     )
 }
