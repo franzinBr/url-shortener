b921a3bba1a42c09ac28125a7fc365be9f89e42b
franzinBr <alan-franzin2011@live.com>
2021-06-06 20:07:23 -0400

    redirecting codes to their respective websites

--- client/src/services/Redirect.js
+++ client/src/services/Redirect.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
+import { URL_GET } from './endpoints'
 
 
 const Redirect = () => {
+    const navigate = useNavigate();
 
     React.useEffect( () => {
-        const code = window.location.pathname.replace('/', '')
-        console.log(code)
-        window.location.href = 'http://www.google.com';
-    }, [])
+        
+        async function getCode()
+        {
+            const code = window.location.pathname.replace('/', '')
+            try {
+                const res = await URL_GET(code)
+                if(res.success === false) throw new Error(res.error)
+                let url = res.data.completeUrl;
+                if (!url.match(/^http?:\/\//i) || !url.match(/^https?:\/\//i)) url = `http://${url}`;
+                window.location.href = url;
+                
+            } catch (error) {
+                navigate('/')
+            }
+        }
+        getCode();
+
+        
+    }, [navigate])
 
     return (
         <>
