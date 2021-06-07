78d97006c3afff42e5c4b3c2ad060dc2536540a5
franzinBr <alan-franzin2011@live.com>
2021-06-06 20:04:52 -0400

    axios interceptor to handle error status codes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,17 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3080/api/v1'
+    baseURL: 'http://localhost:3080/api/v1',
+
 })
+
+api.defaults.withCredentials = true;
+
+
+api.interceptors.response.use(undefined, err => {
+
+    return err.response.data
+});
+
 
 export default api;
