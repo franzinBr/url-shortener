0d54852160584b654525d132b5264bb8b7ba00d2
franzinBr <alan-franzin2011@live.com>
2021-06-07 21:20:23 -0400

    refreshing the token after expiry time

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,48 @@ import Footer from "./components/Footer";
 import User from './pages/User'
 import Home from "./pages/Home";
 import Redirect from "./services/Redirect";
+import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { refresh } from './store/slices/auth';
+import NavPerfil from './components/NavPerfil';
+
 
 
 function App() {
+  
+  const auth = useSelector((state) => state.auth)
+  const dispatch = useDispatch();
+
+
+  React.useEffect(() => {
+    let expiresIn;
+    let timeout;
+
+    async function refreshToken()
+    {
+      const refreshed = await dispatch(refresh())
+      if(refreshed.success) 
+      {
+        expiresIn = (refreshed.exp * 1000) - new Date().getTime()
+        clearTimeout(timeout)
+        timeout = setTimeout(refreshToken, expiresIn)
+        return
+      }
+  
+    }
+    refreshToken()
+
+    
+    return () => clearTimeout(timeout)
+  }, [dispatch, auth.logged])
+
+
   return (
     <div className="App">
       <BrowserRouter>
       <Header />
       <main className="AppMain">
+        <NavPerfil />
         <Routes>
           <Route path="/" element={ <Home />}/>
           <Route path="/user/*" element={ <User />}/>
