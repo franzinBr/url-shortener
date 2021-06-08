1da1eeca69805d7dcea71ae07e97004776342eae
franzinBr <alan-franzin2011@live.com>
2021-06-08 17:45:00 -0400

    remake NavPerfil component

--- client/src/components/NavPerfil/index.js
+++ client/src/components/NavPerfil/index.js
@@ -4,6 +4,7 @@ import styles from './styles.module.css'
 import {ReactComponent as Login} from '../../assets/login.svg'
 import {ReactComponent as Register} from '../../assets/register.svg'
 import {ReactComponent as Logout} from '../../assets/logout.svg'
+import {ReactComponent as User} from '../../assets/user.svg'
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../../store/slices/auth'
 
@@ -15,10 +16,17 @@ const NavPerfil = () => {
 
     return (
         <section className={styles.subHeader}>
-            <nav className={styles.options}>
-                {!auth.data?.authToken && <> <Link to="/user"><Login />Login</Link>
-                <Link to="/user/register"><Register />Register</Link> </>}
-                {auth.data?.authToken && <button onClick={() => dispatch(logout())}><Logout/>Logout</button>}
+            <nav className={styles.userMenu}>
+                <ul className={styles.menuList}>
+                    <li className={styles.dropdown}>
+                        <span className={styles.spanName}><User /><p>Account</p></span>
+                        <ul className={styles.dropdownMenu}>
+                            {!auth.logged && <><li><Link to="/user"><Login />Login</Link></li>
+                            <li><Link to="/user/register"><Register />Register</Link></li></>}
+                            {auth.logged && <li><button onClick={() => dispatch(logout())}><Logout/>Logout</button></li>}
+                        </ul>
+                    </li>
+                </ul>
             </nav>
         </section>
     )
