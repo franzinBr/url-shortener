3fe04126d797e9ec73b3cc6a72f86ee38a083019
franzinBr <alan-franzin2011@live.com>
2021-06-09 19:41:31 -0400

    created option to hide and show passwords

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import styles from './styles.module.css'
+import {ReactComponent as Hide} from '../../assets/hide.svg'
+import {ReactComponent as Show} from '../../assets/show.svg'
 
 const Input = ({type, name, label, value, onChange, error, onBlur}) => {
+
+    const [passwordType, setPasswordType] = React.useState('password')
+
+    function swap()
+    {
+        if(passwordType === 'password') return setPasswordType('text')
+        return setPasswordType('password')
+    }
+
     return (
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper} ${type === "password" ? styles.password : ''}`}>
             <label className={styles.label} htmlFor={name}>
+
                 <input 
                 className={styles.input}
                 id={name}
                 name={name}
-                type={type}
+                type={type !== "password" ? type : passwordType}
                 value={value}
                 onChange={onChange}
                 onBlur={onBlur}
@@ -17,6 +29,7 @@ const Input = ({type, name, label, value, onChange, error, onBlur}) => {
                 />
                 <span className={styles.placeholder}>{label}</span>
             </label>
+            {type === "password" && <span onClick={swap} className={styles.hide}>{passwordType === "password" ? <Show /> : <Hide />}</span>}
             {error && <p>{error}</p>}
         </div>
     )
