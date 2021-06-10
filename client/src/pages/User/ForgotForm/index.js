310b862646f622aa5383f08f1a0fc3ca32e3b0ab
franzinBr <alan-franzin2011@live.com>
2021-06-10 17:42:36 -0400

    component to send password reset emails

--- client/src/pages/User/ForgotForm/index.js
+++ client/src/pages/User/ForgotForm/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import Button from '../../../components/Button'
 import Input from '../../../components/Input'
+import useForm from '../../../hooks/useForm'
+import { FORGOTPASSWORD_POST } from '../../../services/endpoints'
+import styles from './styles.module.css'
 
 const ForgotForm = () => {
+
+    const email = useForm('email');
+    const [showMessage, setShowMessage] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+
+    async function sendEmailPassword(event)
+    {
+        event.preventDefault();
+        if(email.validate())
+        {
+            setLoading(true)
+            await FORGOTPASSWORD_POST({email: email.value})
+            setLoading(false)
+            email.setValue('')
+            setShowMessage(true)
+        }
+    }
+
     return (
-        <div>
-            <Input type="email" label="email" name="email" />
-            <Button>Send Email</Button>
-        </div>
+        <form onSubmit={sendEmailPassword}>
+            <Input type="email" label="email" name="email" {...email} />
+            {!loading ? <Button>Send Email</Button> : <Button disabled>Send Email...</Button>}
+            {showMessage && <p className={styles.message}>If this email is registered on our servers, you will receive an email within the next few minutes with instructions to reset the password</p>}
+        </form>
     )
 }
 
